d34e903996b8c54eb259476e5c2c02a8954cd424
Tobias Maier <tobias.to.maier@deutschebahn.com>
2021-10-07 21:08:25 +0200

    euler 25

--- solutions/euler25.py
+++ solutions/euler25.py
@@ -1,4 +1,8 @@
+"""
+The 12th term, F12, is the first term to contain three digits.
 
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
 
 def fib():
     prev = 1
