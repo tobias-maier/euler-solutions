b7c18187186f8d3800c084d19e3e70e1bc91a542
Tobias Maier <tobias.to.maier@deutschebahn.com>
2021-10-15 22:19:01 +0200

    euler 26

--- solutions/euler26.py
+++ solutions/euler26.py
@@ -1,37 +1,37 @@
-from math import floor
+"""
+A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
 
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7
+has a 6-digit recurring cycle.
 
-def decimal_digits(n):
-    nominator = 10
-    fraction_digits = []
-    r = []
-    c_length = 0
-    while nominator != 0 and c_length == 0:
-        d = floor(nominator / n)
-        fraction_digits.append(d)
-        nominator = 10 * (nominator - d * n)
-        c_length = cycle_length(fraction_digits)
-    if c_length == 0:
-        fraction_digits.extend(r)
-        return ''.join(map(str, fraction_digits))
-    else:
-        return 'r' + ''.join(map(str, fraction_digits[:-c_length]))
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+"""
+from decimal import *
 
+def rec_length(s):
+    for candidate in range(1, precision):
+        if s[-candidate:] == s[-2 * candidate: -candidate]:
+            return candidate
+    return precision + 1
 
-def cycle_length(digits):
-    for i in range(1, int(len(digits)/2) + 1):
-        if digits[-i:] == digits[-2 * i: -i] and set(digits[-i:]) != {0}:
-            return i
-    return 0
+precision = 2000
+getcontext().prec = precision
+count = 0
+max_length, max_d = 0, 1
+for d in range(2, 1000):
+    f = Decimal(1) / Decimal(d)
+    if len(str(f)[2:]) >= precision:
+        cycle_lenght = rec_length(str(f)[2:-10])
+        if cycle_lenght > max_length:
+            max_length, max_d = cycle_lenght, d
 
-
-max_lenght = 0
-index_of_max_lenght = -1
-for i in range(2, 1001):
-    reciprocal = decimal_digits(i)
-    print(f"1/{i}={reciprocal} --> {len(reciprocal) - 1}")
-    if reciprocal.startswith("r") and len(reciprocal) - 1 > max_lenght:
-        max_lenght = len(reciprocal) - 1
-        index_of_max_lenght = i
-
-print(f"l({index_of_max_lenght})={max_lenght}")
+print(f"max_length={max_length}, max_d={max_d}")
